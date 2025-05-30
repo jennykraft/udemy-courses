2e9530141bb79787fabfd5c43b74cfd470235935
Jenny Kraft <jenny.kraft@sap.com>
2025-05-30 16:49:26 +0200

    arrow vs regular functions

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -98,3 +98,113 @@ matilda.calcAge(); // works, returns 5
 const f = jonas.calcAge; // copy the function into a new variable
 
 //f(); // this is undefined so you get an error
+
+
+
+
+console.log("Regular functions vs arrow functions");
+
+var firstName2 = 'Matilda'; // -> don't use var
+
+const jonas2 = {
+    firstName2: 'Jonas',
+    year: 1999,
+    calcAge: function () {
+        console.log(this);
+        console.log(2025 - this.year);
+    },
+
+    greet: () => console.log(`Hey ${this.firstName2}`),
+};
+
+jonas2.greet(); // undefined because of arrow function not getting its own this keyword, but only the parent's (global scope)
+// with var firstName you get "Hey Matilda"
+
+//NEVER USE AN ARROW FUNCTION AS A METHOD
+
+
+const anna = {
+    firstName: 'Anna',
+    year: 1999,
+    calcAge: function () {
+        console.log(this);
+        console.log(2025 - this.year);
+
+        const isMilleanial = function () {
+            console.log(this);
+            console.log(this.year >= 1981 && this.year <= 1996);
+        };
+        isMilleanial();
+    },
+
+    greet: () => console.log(`Hey ${this.firstName}`),
+};
+
+//anna.calcAge(); // error because this undefined in isMillenial()
+
+// pre ES6 solution: variable self
+
+const anna2 = {
+    firstName: 'Anna',
+    year: 1990,
+    calcAge: function () {
+        console.log(this);
+        console.log(2025 - this.year);
+
+        const self = this; // self or that 
+
+        const isMilleanial = function () {
+            console.log(self);
+            console.log(self.year >= 1981 && self.year <= 1996);
+        };
+        isMilleanial();
+    },
+
+    greet: () => console.log(`Hey ${this.firstName}`),
+};
+
+anna2.calcAge();
+
+
+
+// ES6 solution: use an arrow function
+
+const anna3 = {
+    firstName: 'Anna',
+    year: 1990,
+    calcAge: function () {
+        console.log(this);
+        console.log(2025 - this.year);
+
+
+        const isMilleanial = () => {
+            console.log(this);
+            console.log(this.year >= 1981 && this.year <= 1996); // this from the parent scope -> Anna object
+        };
+        isMilleanial();
+    },
+
+    greet: () => console.log(`Hey ${this.firstName}`),
+};
+
+anna3.calcAge();
+
+
+
+console.log("arguments keyword") // only available in regular functions
+
+const addExpr2 = function (a, b) {
+    console.log(arguments);
+    return a + b;
+};
+
+addExpr2(2, 5);
+addExpr2(2, 5, 8, 12);
+
+var addArrow2 = (a, b) => {
+    console.log(arguments);
+    return a + b; // arrow function doesn't get arguments
+}
+
+// addArrow2(2, 5, 8); // error
+
